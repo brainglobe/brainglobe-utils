2d43f0e014bac44dd184694517dbd4ebcf4d4bb7
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-24 11:54:20 +0000

    init

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = ["pandas"]
+
+
+setup(
+    name="imlib",
+    version="0.0.1a",
+    description="General image processing functions",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "sphinx",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "pydoc-markdown",
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coveralls",
+            "coverage<=4.5.4",
+        ]
+    },
+    python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=("docs", "tests*")),
+    include_package_data=True,
+    url="https://github.com/adamltyson/imlib",
+    author="Adam Tyson",
+    author_email="adam.tyson@ucl.ac.uk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
