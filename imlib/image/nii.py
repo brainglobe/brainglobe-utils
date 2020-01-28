67cc3195071579592bf3de96c9729af5d4ea3b66
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-28 16:52:29 +0000

    add pixel size param to get_transformation_matrix

--- imlib/image/nii.py
+++ imlib/image/nii.py
@@ -9,13 +9,16 @@ def get_atlas_pixel_sizes(atlas_config_path):
     return atlas_pixel_sizes
 
 
-def get_transformation_matrix(atlas_config):
+def get_transformation_matrix(atlas_config=None, pixel_sizes=None):
     """
     From an atlas config, return transformation_matrix for proper nifti output
     :param atlas_config:
     :return: transformation_matrix
     """
-    atlas_pixel_sizes = get_atlas_pixel_sizes(atlas_config)
+    if pixel_sizes is None:
+        atlas_pixel_sizes = get_atlas_pixel_sizes(atlas_config)
+    else:
+        atlas_pixel_sizes = pixel_sizes
     transformation_matrix = np.eye(4)
     for i, axis in enumerate(("x", "y", "z")):
         transformation_matrix[i, i] = atlas_pixel_sizes[axis]
--- tests/tests/test_unit/test_image/test_nii.py
+++ tests/tests/test_unit/test_image/test_nii.py
@@ -8,6 +8,7 @@ data_dir = Path("tests", "data")
 config_file = data_dir / "nii" / "conf.conf"
 
 atlas_pixel_sizes = {"x": "10", "y": "10", "z": "10"}
+
 transformation_matrix = np.array(
     ([10, 0, 0, 0], [0, 10, 0, 0], [0, 0, 10, 0], [0, 0, 0, 1])
 )
@@ -19,4 +20,10 @@ def test_get_atlas_pixel_sizes():
 
 def test_get_transformation_matrix():
     test_transformation_matrix = nii.get_transformation_matrix(config_file)
+    assert (test_transformation_matrix == transformation_matrix).all()
+
+    test_transformation_matrix = nii.get_transformation_matrix(
+        atlas_config=None, pixel_sizes=atlas_pixel_sizes
+    )
+
     assert (test_transformation_matrix == transformation_matrix).all()
