e1b29c4eebc66185e23efcf0b478eb955fc1408e
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-02-04 17:39:14 +0000

    image reorientation

---
+++ imlib/image/orient.py
@@ -0,0 +1,23 @@
+def reorient_image(image, invert_axes=None, orientation="saggital"):
+    """
+    Reorients the image to the coordinate space of the atlas
+
+    :param image_path: str
+    :param threshold: float
+    :param invert_axes: tuple (Default value = None)
+    :param image:
+    :param orientation:  (Default value = "saggital")
+
+    """
+    if invert_axes is not None:
+        for axis in list(invert_axes):
+            image = np.flip(image, axis=axis)
+
+    if orientation is not "saggital":
+        if orientation is "coronal":
+            transposition = (2, 1, 0)
+        elif orientation is "horizontal":
+            transposition = (1, 2, 0)
+
+        image = np.transpose(image, transposition)
+    return image
