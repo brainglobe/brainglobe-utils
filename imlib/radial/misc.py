7f3b7b48e6b08389f34cb05b223c051b50d96390
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-02 18:23:29 +0100

    add unwrap

--- imlib/radial/misc.py
+++ imlib/radial/misc.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def opposite_angle_deg(angle):
     """
     Returns the "opposite" angle. i.e 180 deg away from the input angle
@@ -5,3 +8,18 @@ def opposite_angle_deg(angle):
     :return: "Opposite angle"
     """
     return 180 - abs(abs(angle - 180) - 180)
+
+
+def phase_unwrap(degrees, discontinuity=180):
+    """
+    Unwraps phase discontinuity (i.e. adds or subtracts 180 deg when 0/360
+    degrees is passed.
+    :param degrees: Input angles in degrees
+    :param discontinuity: Cut off of discontinuity to assume a phase wrap.
+    Default: 180 degrees
+    :return np.array: Unwrapped angles
+    """
+    discontinuity = np.deg2rad(discontinuity)
+    rad = np.deg2rad(degrees)
+    unwrap = np.unwrap(rad, discont=discontinuity)
+    return np.rad2deg(unwrap)
