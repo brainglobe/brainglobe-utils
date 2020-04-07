46e4291e5482a668f320b2ce68dd0c326dbd9c4d
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-07 14:28:54 +0100

    fix bug with finding the opposite angle

--- imlib/radial/misc.py
+++ imlib/radial/misc.py
@@ -1,13 +1,49 @@
 import numpy as np
 
+pi = np.pi
 
-def opposite_angle_deg(angle):
+
+def radial_bins(bin_size, degrees=True, remove_greater_than_360=False):
+    """
+    Given a bin size (width), divide a 360 degree radial distribution into
+    bins (defined by edges). Only works properly when an integer number
+    of bins fit into 360 degrees
+    :param bin_size: Wdith of each bin
+    :param degrees: If True, work in degrees, otherwise radians
+    :param remove_greater_than_360: If there is not an integer number of
+    bins in 360 degrees, what do to do with the last (>360 degree) bin.
+    :return: np.array of bin edges
+    """
+    if degrees:
+        bins = np.arange(0, 360 + bin_size, bin_size)
+        max_permitted = 360
+    else:
+        bins = np.arange(0, 2 * pi + bin_size, bin_size)
+        max_permitted = 2 * pi
+
+    if remove_greater_than_360:
+        if bins[-1] > max_permitted:
+            bins = bins[0:-1]
+    return bins
+
+
+def opposite_angle(angle, degrees=True):
     """
     Returns the "opposite" angle. i.e 180 deg away from the input angle
     :param angle: Input angle in degrees
+    :param degrees: If True, work in degrees, otherwise radians
     :return: "Opposite angle"
     """
-    return 180 - abs(abs(angle - 180) - 180)
+    if degrees:
+        if angle > 180:
+            return angle - 180
+        else:
+            return angle + 180
+    else:
+        if angle > pi:
+            return angle - pi
+        else:
+            return angle + pi
 
 
 def phase_unwrap(degrees, discontinuity=180):
