8208d0cb0380261d639922b5876ab0be09dcb775
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-04-24 12:09:44 +0100

    remove python 3.8 support

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             "coverage<=4.5.4",
         ]
     },
-    python_requires=">=3.6",
+    python_requires=">=3.6, <3.8",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
     url="https://github.com/adamltyson/imlib",
