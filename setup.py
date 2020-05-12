8f43f61fcec8a009b0d863727b3b3b89f6a07986
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-05-12 16:41:21 +0100

    remove py38 testing

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
             "coverage",
         ]
     },
-    python_requires=">=3.6",
+    python_requires=">=3.6, <3.8",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
     url="https://github.com/adamltyson/imlib",
