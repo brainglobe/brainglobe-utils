6d77c216f23a47e754bee1d04b7c856baf803b42
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-05-12 16:37:47 +0100

    add py38 testing

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
             "coverage",
         ]
     },
-    python_requires=">=3.6, <3.8",
+    python_requires=">=3.6",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
     url="https://github.com/adamltyson/imlib",
