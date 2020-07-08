ba5f8453789e38138dcbd5b2998aafcb3d642742
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-07-08 11:12:11 +0100

    bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name="imlib",
-    version="0.0.26rc0",
+    version="0.0.26",
     description="General data processing functions",
     install_requires=requirements,
     extras_require={
