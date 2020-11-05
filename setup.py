9a77dcd432e76ea40a1848f036209b5c29587e31
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-11-05 11:15:29 +0000

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requirements = [
 
 setup(
     name="imio",
-    version="0.0.3",
+    version="0.0.4",
     description="Loading and saving of image data.",
     install_requires=requirements,
     extras_require={
