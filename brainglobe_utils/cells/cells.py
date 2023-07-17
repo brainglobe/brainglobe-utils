575ee131d25fef399ddf5d1f44b54278b649a838
willGraham01 <1willgraham@gmail.com>
2023-07-17 13:51:10 +0100

    Ruff linting

--- brainglobe_utils/cells/cells.py
+++ brainglobe_utils/cells/cells.py
@@ -361,8 +361,8 @@ def group_cells_by_z(cells: List[Cell]) -> defaultdict[float, List[Cell]]:
     For a list of Cells return a dict of lists of cells, grouped by plane.
 
     :param list cells: list of cells from cellfinder.cells.cells.Cell
-    :return:  defaultdict, with each key being a plane (e.g. 1280) and each entry being a list
-    of Cells
+    :return:  defaultdict, with each key being a plane (e.g. 1280)
+    and each entry being a list of Cells
     """
     cells_groups = defaultdict(list)
     for cell in cells:
