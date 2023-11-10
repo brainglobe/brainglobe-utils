edfc5291770bd7b2a7851df42cdfc4d253c02e10
IgorTatarnikov <igor.tatarnikov@gmail.com>
2023-11-10 15:20:29 +0000

    Version 1 of qtpy logo.py and tests

--- brainglobe_utils/qtpy/logo.py
+++ brainglobe_utils/qtpy/logo.py
@@ -4,11 +4,14 @@ from qtpy.QtWidgets import QGroupBox, QHBoxLayout, QLabel, QWidget
 
 
 def _docs_links_widget(
-    package_name: str, tutorial_file_name: str, parent: QWidget = None
+    package_name: str,
+    package_tagline: str,
+    tutorial_file_name: str,
+    parent: QWidget = None,
 ):
     _docs_links_html = f"""
     <h3>
-    <p>2D Registration</p>
+    <p>{package_tagline}</p>
     <p><a href="https://brainglobe.info" style="color:gray;">Website</a></p>
     <p><a href="https://brainglobe.info/tutorials/{tutorial_file_name}" style="color:gray;">Tutorial</a></p>
     <p><a href="https://github.com/brainglobe/{package_name}" style="color:gray;">Source</a></p>
@@ -20,31 +23,44 @@ def _docs_links_widget(
 
 
 def _logo_widget(package_name: str, parent: QWidget = None):
-    brainglobe_logo = files("brainglobe.png")
+    brainglobe_logo = files("brainglobe_utils").joinpath("qtpy/brainglobe.png")
 
     _logo_html = f"""
     <h1>
     <img src="{brainglobe_logo}"width="100">
     <p>{package_name}</p>
     <\h1>
-    """
+    """  # noqa W605
 
     return QLabel(_logo_html, parent=None)
 
 
 def header_widget(
-    package_name: str, tutorial_file_name: str, parent: QWidget = None
+    package_name: str,
+    package_tagline: str,
+    tutorial_file_name: str,
+    parent: QWidget = None,
 ):
     """
     Render HTML in a QGroupBox with a BrainGlobe logo and links to the docs
      and source code.
-    Args:
-        package_name: The name of the package, e.g. "brainrender-napari"
-        tutorial_file_name: The name of the tutorial file
-            (must include .html), e.g. "brainrender-napari.html"
-        parent: The parent widget
 
-    Returns: A QGroupBox with the rendered HTML containing the logo and links
+    Parameters
+    ----------
+    package_name : str
+        The name of the package, e.g. "brainrender-napari"
+    package_tagline : str
+        The tagline for the package
+    tutorial_file_name : str
+        The name of the tutorial file (must include .html),
+         e.g. "brainrender-qtpy.html": str
+    parent : QWidget
+        The parent widget, defaults to None
+
+    Returns
+    -------
+    QGroupBox
+        A QGroupBox with the rendered HTML containing the logo and links
     """
 
     box = QGroupBox(parent)
@@ -52,7 +68,9 @@ def header_widget(
     box.setLayout(QHBoxLayout())
     box.layout().addWidget(_logo_widget(package_name, parent=box))
     box.layout().addWidget(
-        _docs_links_widget(package_name, tutorial_file_name, parent=box)
+        _docs_links_widget(
+            package_name, package_tagline, tutorial_file_name, parent=box
+        )
     )
 
     return box
