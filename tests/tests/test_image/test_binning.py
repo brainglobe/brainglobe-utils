cb4832967dae5903b7d524f698048e678bb51fb1
Adam Tyson <code@adamltyson.com>
2024-01-08 09:20:13 +0000

    Brainmapper utils (#22)
    * copy binning bugfix from brainmapper
    
    * Add heatmap functionality from brainmapper
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update dependencies
    
    * Update path to test data
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/tests/test_image/test_binning.py
+++ tests/tests/test_image/test_binning.py
@@ -7,9 +7,9 @@ def test_get_bins():
     image_size = (1000, 20, 125, 725)
     bin_sizes = (500, 2, 25, 100)
 
-    dim0_bins = np.array((0, 500))
-    dim1_bins = np.array((0, 2, 4, 6, 8, 10, 12, 14, 16, 18))
-    dim2_bins = np.array((0, 25, 50, 75, 100))
+    dim0_bins = np.array((0, 500, 1000))
+    dim1_bins = np.array((0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20))
+    dim2_bins = np.array((0, 25, 50, 75, 100, 125))
     dim3_bins = np.array((0, 100, 200, 300, 400, 500, 600, 700))
     bins = binning.get_bins(image_size, bin_sizes)
 
