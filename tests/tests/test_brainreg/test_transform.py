5510e14014d15e4bd3d6a4be0cd88981c01c5f48
alessandrofelder <alessandrofelder@users.noreply.github.com>
2024-02-23 11:29:26 +0000

    parametrise test

--- tests/tests/test_brainreg/test_transform.py
+++ tests/tests/test_brainreg/test_transform.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import numpy as np
+import pytest
 from bg_atlasapi import BrainGlobeAtlas
 
 from brainglobe_utils.brainreg.transform import (
@@ -8,11 +9,27 @@ from brainglobe_utils.brainreg.transform import (
 )
 
 
-def test_transform_points_from_downsampled_to_atlas_space(mocker):
+@pytest.mark.parametrize(
+    (
+        "mock_deformation_field",
+        "expected_transformed_points",
+        "expected_points_out_of_bounds",
+    ),
+    [
+        (np.ones((132, 80, 114)), [[10, 10, 10], [10, 10, 10]], []),
+        (np.ones((4, 4, 4)), np.atleast_3d([]), [[5, 5, 5], [6, 6, 6]]),
+    ],
+)
+def test_transform_points_from_downsampled_to_atlas_space(
+    mocker,
+    mock_deformation_field,
+    expected_transformed_points,
+    expected_points_out_of_bounds,
+):
     """
     Test case for transforming points from downsampled space to atlas space.
-    This is a basic test case that covers a deformation field that maps
-    everything to [1, 1, 1].
+    * check that deformation field of ones maps to 1,1,1*resolution
+    * check that too small deformation field maps points to out-of-bounds
 
     Args:
         mocker: The mocker object used to patch the reading of deformation
@@ -21,14 +38,11 @@ def test_transform_points_from_downsampled_to_atlas_space(mocker):
     Returns:
         None
     """
-    mock_deformation_field = np.ones(
-        (132, 80, 114)
-    )  # shape of allen 100 reference, has unit mm
     mocker.patch(
         "brainglobe_utils.brainreg.transform.tifffile.imread",
         side_effect=lambda x: mock_deformation_field,
     )
-    downsampled_points = np.array([[1, 1, 1], [2, 2, 2]])
+    downsampled_points = np.array([[5, 5, 5], [6, 6, 6]])
     transformed_points, points_out_of_bounds = (
         transform_points_from_downsampled_to_atlas_space(
             downsampled_points=downsampled_points,
@@ -42,44 +56,5 @@ def test_transform_points_from_downsampled_to_atlas_space(mocker):
     )
     # because we mock the deformation field as all ones,
     # all coordinates should be mapped to [1,1,1]*1mm/100um = [10,10,10]
-    assert np.all(transformed_points == np.ones_like(transformed_points) * 10)
-    assert not points_out_of_bounds, str(points_out_of_bounds)
-
-
-def test_transform_points_from_downsampled_to_atlas_space_out_of_bounds(
-    mocker,
-):
-    """
-    Test case for transforming points from downsampled space to atlas space
-    when points are out of bounds.
-    Points are out of bounds when they cause an index error on the deformation
-    field.
-
-    Args:
-        mocker: The mocker object to patch the reading of deformation
-                field tiffs
-
-    Returns:
-        None
-    """
-    mock_deformation_field = np.ones((4, 4, 4))
-    mocker.patch(
-        "brainglobe_utils.brainreg.transform.tifffile.imread",
-        side_effect=lambda x: mock_deformation_field,
-    )
-    downsampled_points = np.array([[5, 5, 5]])
-    transformed_points, points_out_of_bounds = (
-        transform_points_from_downsampled_to_atlas_space(
-            downsampled_points=downsampled_points,
-            atlas=BrainGlobeAtlas("allen_mouse_100um"),
-            deformation_field_paths=[
-                Path.home() / "dummy_x_deformation.tif",
-                Path.home() / "dummy_y_deformation.tif",
-                Path.home() / "dummy_z_deformation.tif",
-            ],
-        )
-    )
-
-    assert len(transformed_points) == 0
-    assert len(points_out_of_bounds) == 1
-    assert points_out_of_bounds[0] == [5, 5, 5]
+    assert np.all(transformed_points == expected_transformed_points)
+    assert points_out_of_bounds == expected_points_out_of_bounds
