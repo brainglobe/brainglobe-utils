4bb3d2e7c240005e81786bd8a7ccd30fcfb1216d
Kimberly Meechan <k.meechan@ucl.ac.uk>
2024-03-04 17:54:11 +0000

    convert image_io docstrings to numpydoc style

--- brainglobe_utils/image_io/utils.py
+++ brainglobe_utils/image_io/utils.py
@@ -8,15 +8,24 @@ class ImageIOLoadException(Exception):
 
 def check_mem(img_byte_size, n_imgs):
     """
-    Check how much memory is available on the system and compares it to the
+    Checks how much memory is available on the system and compares it to the
     size the stack specified by img_byte_size and n_imgs would take
     once loaded
 
     Raises an error in case memory is insufficient to load that stack
 
-    :param int img_byte_size: The size in bytes of an individual image plane
-    :param int n_imgs: The number of image planes to load
-    :raises: BrainLoadException if not enough memory is available
+    Parameters
+    ----------
+    img_byte_size : int
+        The size in bytes of an individual image plane.
+
+    n_imgs : int
+        The number of image planes to load.
+
+    Raises
+    ------
+    BrainLoadException
+        If not enough memory is available.
     """
     total_size = img_byte_size * n_imgs
     free_mem = psutil.virtual_memory().available
@@ -29,11 +38,15 @@ def check_mem(img_byte_size, n_imgs):
 
 def scale_z(volume, scaling_factor):
     """
-    Scale the given brain along the z dimension
+    Scale the given brain along the z dimension.
 
-    :param np.ndarray volume: A brain typically as a numpy array
-    :param float scaling_factor:
-    :return:
+    Parameters
+    ----------
+    volume : np.ndarray
+        A brain, typically as a numpy array.
+
+    scaling_factor : float
+        The z scaling factor.
     """
 
     return zoom(volume, (scaling_factor, 1, 1), order=1)
