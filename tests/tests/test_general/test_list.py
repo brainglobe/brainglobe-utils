830c1d2892f325fd37d05141cadc6a2d97bb8663
Kimberly Meechan <k.meechan@ucl.ac.uk>
2024-03-21 15:20:41 +0000

    refactor fixtures

--- tests/tests/test_general/test_list.py
+++ tests/tests/test_general/test_list.py
@@ -3,26 +3,16 @@ import pytest
 from brainglobe_utils.general import list as list_tools
 
 
-@pytest.fixture
-def a():
-    return [1, "a", 10, 30]
-
-
-@pytest.fixture
-def b():
-    return [30, 10, "c", "d"]
-
-
-@pytest.fixture
-def list_with_empty():
-    return ["test1", "test 2", " ", "", "test4", ""]
-
-
-@pytest.fixture
-def list_without_empty():
-    return ["test1", "test 2", " ", "test4"]
-
-
+@pytest.mark.parametrize(
+    "list_with_empty, list_without_empty",
+    [
+        pytest.param(
+            ["test1", "test 2", " ", "", "test4", ""],
+            ["test1", "test 2", " ", "test4"],
+            id="remove empty string",
+        ),
+    ],
+)
 def test_remove_empty_string(list_with_empty, list_without_empty):
     assert (
         list_tools.remove_empty_string(list_with_empty) == list_without_empty
@@ -42,5 +32,11 @@ def test_check_unique_list():
     assert (False, [3, "dog"]) == list_tools.check_unique_list(repeating_list)
 
 
+@pytest.mark.parametrize(
+    "a, b",
+    [
+        pytest.param([1, "a", 10, 30], [30, 10, "c", "d"], id="common 10 30"),
+    ],
+)
 def test_common_member(a, b):
     assert (True, [10, 30]) == list_tools.common_member(a, b)
