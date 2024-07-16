eed0df4279efa36c70839a22cd7d4242e823c080
Adam Tyson <code@adamltyson.com>
2024-07-16 15:00:50 +0100

    Add analysis and saving

--- brainglobe_utils/brainreg/napari.py
+++ brainglobe_utils/brainreg/napari.py
@@ -7,14 +7,19 @@ import tifffile
 from brainglobe_atlasapi import BrainGlobeAtlas
 from brainglobe_space import AnatomicalSpace
 from qtpy import QtCore
+from qtpy.QtCore import QAbstractTableModel, Qt
 from qtpy.QtWidgets import (
     QComboBox,
     QFileDialog,
     QGridLayout,
     QLabel,
+    QTableView,
     QWidget,
 )
 
+from brainglobe_utils.brainmapper.analysis import (
+    summarise_points_by_atlas_region,
+)
 from brainglobe_utils.brainreg.transform import (
     transform_points_from_downsampled_to_atlas_space,
 )
@@ -28,6 +33,7 @@ class TransformPoints(QWidget):
         super(TransformPoints, self).__init__()
         self.viewer = viewer
         self.raw_data = None
+        self.points_layer = None
         self.atlas = None
         self.transformed_points = None
 
@@ -88,14 +94,15 @@ class TransformPoints(QWidget):
         self.layout.setAlignment(QtCore.Qt.AlignTop)
         self.layout.setSpacing(4)
         self.add_header()
-        self.add_brainreg_load_button(row=1, column=0)
-        self.add_points_combobox(row=2, column=0)
-        self.add_raw_data_combobox(row=3, column=0)
-        self.add_transform_button(row=4, column=0)
+        self.add_points_combobox(row=1, column=0)
+        self.add_raw_data_combobox(row=2, column=0)
+        self.add_transform_button(row=3, column=0)
+        self.add_analyse_button(row=3, column=1)
 
-        self.status_label = QLabel()
-        self.status_label.setText("Ready")
-        self.layout.addWidget(self.status_label, 5, 0)
+        self.add_points_summary_table(row=4, column=0)
+        self.add_save_all_points_button(row=6, column=0)
+        self.add_save_points_summary_button(row=6, column=1)
+        self.add_status_label(row=7, column=0)
 
         self.setLayout(self.layout)
 
@@ -114,21 +121,10 @@ class TransformPoints(QWidget):
         )
         self.layout.addWidget(header, 0, 0, 1, 2)
 
-    def add_brainreg_load_button(self, row, column):
-        self.load_button = add_button(
-            "Load brainreg directory",
-            self.layout,
-            self.load_brainreg_directory,
-            row=row,
-            column=column,
-            visibility=True,
-            tooltip="Choose a brainreg project directory",
-        )
-
     def add_points_combobox(self, row, column):
         self.points_layer_choice, _ = add_combobox(
             self.layout,
-            "Points",
+            "Points layer",
             self.points_layer_names,
             column=column,
             row=row,
@@ -138,7 +134,7 @@ class TransformPoints(QWidget):
     def add_raw_data_combobox(self, row, column):
         self.raw_data_choice, _ = add_combobox(
             self.layout,
-            "Raw data",
+            "Raw data layer",
             self.image_layer_names,
             column=column,
             row=row,
@@ -156,8 +152,117 @@ class TransformPoints(QWidget):
             tooltip="Transform points layer to atlas space",
         )
 
+    def add_analyse_button(self, row, column):
+        self.analyse_button = add_button(
+            "Analyse points",
+            self.layout,
+            self.analyse_points,
+            row=row,
+            column=column,
+            visibility=False,
+            tooltip="Analyse distribution of points within the atlas",
+        )
+
+    def add_points_summary_table(self, row, column):
+        self.points_per_region_table_title = QLabel(
+            "Points distribution summary"
+        )
+        self.points_per_region_table_title.setVisible(False)
+        self.layout.addWidget(self.points_per_region_table_title, row, column)
+        self.points_per_region_table = QTableView()
+        self.points_per_region_table.setVisible(False)
+        self.layout.addWidget(self.points_per_region_table, row + 1, column)
+
+    def add_save_all_points_button(self, row, column):
+        self.save_all_points_button = add_button(
+            "Save all points information",
+            self.layout,
+            self.save_all_points_csv,
+            row=row,
+            column=column,
+            visibility=False,
+            tooltip="Save all points information as a csv file",
+        )
+
+    def add_save_points_summary_button(self, row, column):
+        self.save_points_summary_button = add_button(
+            "Save points summary",
+            self.layout,
+            self.save_points_summary_csv,
+            row=row,
+            column=column,
+            visibility=False,
+            tooltip="Save points summary as a csv file",
+        )
+
+    def add_status_label(self, row, column):
+        self.status_label = QLabel()
+        self.status_label.setText("Ready")
+        self.layout.addWidget(self.status_label, row, column)
+
+    def set_raw_data_layer(self):
+        """
+        Set background layer from current background text box selection.
+        """
+        if self.raw_data_choice.currentText() != "":
+            self.raw_data = self.viewer.layers[
+                self.raw_data_choice.currentText()
+            ]
+
+    def set_points_layer(self):
+        """
+        Set background layer from current background text box selection.
+        """
+        if self.points_layer_choice.currentText() != "":
+            self.points_layer = self.viewer.layers[
+                self.points_layer_choice.currentText()
+            ]
+
+    def transform_points_to_atlas_space(self):
+        layers_in_place = self.check_layers()
+        if not layers_in_place:
+            return
+
+        self.status_label.setText("Loading brainreg data ...")
+        self.load_brainreg_directory()
+        self.status_label.setText("Transforming points ...")
+
+        self.run_transform_points_to_downsampled_space()
+        self.run_transform_downsampled_points_to_atlas_space()
+        self.analyse_button.setVisible(True)
+        self.status_label.setText("Ready")
+
+    def check_layers(self):
+        if self.raw_data is None and self.points_layer is None:
+            display_info(
+                self,
+                "No layers selected",
+                "Please select the layers corresponding to the points "
+                "you would like to transform and the raw data (registered by "
+                "brainreg)",
+            )
+            return False
+
+        if self.raw_data is None:
+            display_info(
+                self,
+                "No raw data layer selected",
+                "Please select a layer that corresponds to the raw "
+                "data (registered by brainreg)",
+            )
+            return False
+
+        if self.points_layer is None:
+            display_info(
+                self,
+                "No points layer selected",
+                "Please select a points layer you would like to transform",
+            )
+            return False
+
+        return True
+
     def load_brainreg_directory(self):
-        self.status_label.setText("Loading...")
         brainreg_directory = QFileDialog.getExistingDirectory(
             self,
             "Select brainreg directory",
@@ -200,34 +305,6 @@ class TransformPoints(QWidget):
 
     def load_atlas(self):
         self.atlas = BrainGlobeAtlas(self.metadata.atlas_string)
-
-    def set_raw_data_layer(self):
-        """
-        Set background layer from current background text box selection.
-        """
-        if self.raw_data_choice.currentText() != "":
-            self.raw_data = self.viewer.layers[
-                self.raw_data_choice.currentText()
-            ]
-
-    def set_points_layer(self):
-        """
-        Set background layer from current background text box selection.
-        """
-        if self.points_layer_choice.currentText() != "":
-            self.points_layer = self.viewer.layers[
-                self.points_layer_choice.currentText()
-            ]
-
-    def transform_points_to_atlas_space(self):
-        # TODO: check correct layers have been chosen
-        self.status_label.setText("Transforming points ...")
-
-        self.run_transform_points_to_downsampled_space()
-        self.run_transform_downsampled_points_to_atlas_space()
-        self.status_label.setText("Ready")
-
-        pass
 
     def run_transform_points_to_downsampled_space(self):
         downsampled_space = self.get_downsampled_space()
@@ -286,6 +363,64 @@ class TransformPoints(QWidget):
         )
         return source_space
 
+    def analyse_points(self):
+        self.all_points_df, self.points_per_region_df = (
+            summarise_points_by_atlas_region(
+                self.points_layer.data,
+                self.points_in_atlas_space,
+                self.atlas,
+                self.paths.volume_csv_path,
+            )
+        )
+
+        self.populate_summary_table()
+        self.save_all_points_button.setVisible(True)
+        self.save_points_summary_button.setVisible(True)
+
+        print("Analysing points")
+
+    def populate_summary_table(
+        self,
+        columns_to_keep=[
+            "structure_name",
+            "left_cell_count",
+            "right_cell_count",
+        ],
+    ):
+        summary_df = self.points_per_region_df[columns_to_keep]
+        self.points_per_region_table_model = DataFrameModel(summary_df)
+        self.points_per_region_table.setModel(
+            self.points_per_region_table_model
+        )
+        self.points_per_region_table_title.setVisible(True)
+        self.points_per_region_table.setVisible(True)
+
+    def save_all_points_csv(self):
+        self.all_points_csv_path, _ = QFileDialog.getSaveFileName(
+            self,
+            "Choose filename",
+            "",
+            "CSV Files (*.csv)",
+        )
+        self.all_points_csv_path = ensure_extension(
+            self.all_points_csv_path, ".csv"
+        )
+        self.all_points_df.to_csv(self.all_points_csv_path, index=False)
+
+    def save_points_summary_csv(self):
+        self.summary_points_csv_path, _ = QFileDialog.getSaveFileName(
+            self,
+            "Choose filename",
+            "",
+            "CSV Files (*.csv)",
+        )
+        self.summary_points_csv_path = ensure_extension(
+            self.summary_points_csv_path, ".csv"
+        )
+        self.points_per_region_df.to_csv(
+            self.summary_points_csv_path, index=False
+        )
+
     class Paths:
         """
         A single class to hold all file paths that may be used.
@@ -304,6 +439,7 @@ class TransformPoints(QWidget):
                 "deformation_field_2.tiff"
             )
             self.downsampled_image = self.make_filepaths("downsampled.tiff")
+            self.volume_csv_path = self.make_filepaths("volumes.csv")
 
         def make_filepaths(self, filename):
             return self.brainreg_directory / filename
@@ -313,3 +449,35 @@ class TransformPoints(QWidget):
             self.orientation = brainreg_metadata["orientation"]
             self.atlas_string = brainreg_metadata["atlas"]
             self.voxel_sizes = brainreg_metadata["voxel_sizes"]
+
+
+class DataFrameModel(QAbstractTableModel):
+    def __init__(self, df):
+        super().__init__()
+        self._df = df
+
+    def rowCount(self, parent=None):
+        return self._df.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._df.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            return str(self._df.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self._df.columns[section]
+            if orientation == Qt.Vertical:
+                return self._df.index[section]
+        return None
+
+
+def ensure_extension(file_path, extension):
+    path = Path(file_path)
+    if path.suffix != extension:
+        path = path.with_suffix(extension)
+    return path
