c63534eeb84eb03192f133f952f9a1b9d540091c
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-09-01 16:37:36 +0100

    Use main instead of master

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -21,7 +21,7 @@ def test_data_registry():
     """
     registry = pooch.create(
         path=Path.home() / ".brainglobe" / "test_data",
-        base_url="http://gin.swc.ucl.ac.uk/brainglobe/test-data/raw/master/",
+        base_url="http://gin.swc.ucl.ac.uk/brainglobe/test-data/raw/main/",
         registry={
             "cellfinder/cells-z-1000-1050.xml": None,
             "cellfinder/other-cells-z-1000-1050.xml": None,
